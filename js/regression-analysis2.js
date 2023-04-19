c18a6e7f50ec89fb19f24f814ad7b0e2a086f7e2
Juandi12 <123569475+Juandi12@users.noreply.github.com>
2023-04-19 14:25:25 -0500

    AVANCE2

--- js/regression-analysis2.js
+++ js/regression-analysis2.js
@@ -39,12 +39,12 @@ function linearRegression(values_x, values_y) {
 
 //For kWh vs Month
 const months = [1, 2, 3, 4, 5];
-const kwhs = [240, 250, 274, 250, 300];
+const kwhs = [148, 160, 184, 157, 158];
 var l = linearRegression(months, kwhs);
 
 //For Residents vs kWh
-const residents = [1, 2, 3, 5, 5, 6, 3, 8, 3, 5];
-const kWhs = [240, 250, 274, 260, 210, 300, 260, 440, 300, 350];
+const residents = [3, 3, 5, 2, 3, 6, 4, 7, 8, 2];
+const kWhs = [140, 116, 192, 155, 79, 221, 86, 475, 110, 145];
 var r = linearRegression(residents, kWhs);
 
 const trace11 = {
